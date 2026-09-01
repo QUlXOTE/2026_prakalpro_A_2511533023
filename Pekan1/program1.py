fd364a7eeab174e877bea1a175ca79eecc30eee7
fajar <fajar@gmail.com>
2026-09-01 10:23:26 +0700

    praktikum1 alpro selsasa

---
+++ Pekan1/program1.py
@@ -0,0 +1,13 @@
+# program pertama
+"""
+komentar
+dua baris pertama
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ ///\\\\\\")
+print("program ini dibuat oleh" + "dibuat oleh a\n " + "M.Fajar fadhilul zikri dan 251153302")
+      
