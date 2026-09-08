ed561f447fd4d5fd93c87ac7c33b110ace01972f
QUlXOTE <soulland207@gmail.com>
2026-09-08 20:39:19 +0700

    Add files via upload

---
+++ Pekan1/Tugas1_2511533023.py
@@ -0,0 +1,35 @@
+# Modul pekan3 (di Python cukup menggunakan file/folder)
+
+def main():
+    # Input data dari pengguna
+    A1_3023 = int(input("Input angka-1: "))
+    A2_3023 = int(input("Input angka-2: "))
+
+    # Operator Penjumlahan
+    print("operator Penjumlahan")
+    hasil_3023 = A1_3023 + A2_3023
+    print("Hasil =", hasil_3023)
+
+    # Operator Pengurangan
+    print("operator Pengurangan")
+    hasil_3023 = A1_3023 - A2_3023
+    print("Hasil =", hasil_3023)
+
+    # Operator Perkalian
+    print("operator Perkalian")
+    hasil_3023 = A1_3023 * A2_3023
+    print("Hasil =", hasil_3023)
+
+    # Operator Hasil Bagi (pembagian bulat)
+    print("operator hasil bagi")
+    hasil_3023 = A1_3023 // A2_3023  # Gunakan // untuk pembagian bulat seperti pada tipe data 'int' di Java
+    print("Hasil =", hasil_3023)
+
+    # Operator Sisa Bagi (Modulus)
+    print("operator sisa bagi")
+    hasil_3023 = A1_3023 % A2_3023
+    print("Hasil =", hasil_3023)
+
+
+if __name__ == "__main__":
+    main()
